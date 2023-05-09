1d158370b7e26e029cd9b6970377c14fd3ff8388
JMHupfer <jmhupfer@gmail.com>
2023-05-08 18:25:01 -0600

    added notes back to app.get call

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
